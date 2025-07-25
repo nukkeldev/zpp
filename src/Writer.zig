aa7fdac104bf1d23d29771a4c10d7f9cfaafdfe0
nukkeldev <110308379+nukkeldev@users.noreply.github.com>
2025-07-24 23:30:29 -0700

    Pivoting towards producing a zig binding instead of c header as thats a lot less headache type-wise

--- src/Writer.zig
+++ src/Writer.zig
@@ -1,2 +1,8 @@
 //! Given the results of `read.zig`'s parsing, outputs an `extern "C"` compatible .cpp file for FFI
 //! usage.
+//! 
+//! > Why generate zig code with extern functions instead of a c header via translate-c?
+//! > Ultimately it comes down to not having to re-create the C++ types in the C header,
+//! > having more control over the output (i.e. associated methods), and just an overall
+//! > want to stay as zig-centric as possible.
+
