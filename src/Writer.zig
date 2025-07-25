e876945407e5ad01bbb214ee6815ab484fc833b2
nukkeldev <110308379+nukkeldev@users.noreply.github.com>
2025-07-24 23:40:04 -0700

    Restructured to better reflect usage

--- src/Writer.zig
+++ src/Writer.zig
@@ -4,5 +4,6 @@
 //! > Why generate zig code with extern functions instead of a c header via translate-c?
 //! > Ultimately it comes down to not having to re-create the C++ types in the C header,
 //! > having more control over the output (i.e. associated methods), and just an overall
-//! > want to stay as zig-centric as possible.
+//! > want to stay as zig-centric as possible. As well as only having to meet an ABI
+//! > requirement for function parameters and return types.
 
