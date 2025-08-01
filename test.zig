7958d68954fb0a452ea969f38c0d8c9b0b7d721f
nukkeldev <110308379+nukkeldev@users.noreply.github.com>
2025-08-01 16:47:55 -0700

    example_null kinda works, very hacky, time to do zig-gen

--- test.zig
+++ test.zig
@@ -4,25 +4,39 @@ const std = @import("std");
 
 const print = std.debug.print;
 
-extern fn ImGui_CreateContext(shared_font_atlas: ?*anyopaque) void;
-extern fn ImGui_DestroyContext(ctx: ?*anyopaque) void;
+extern fn ImGui_CreateContext(shared_font_atlas: ?*anyopaque) callconv(.c) void;
+extern fn ImGui_DestroyContext(ctx: ?*anyopaque) callconv(.c) void;
 
-extern fn ImGui_NewFrame() void;
-extern fn ImGui_Render() void;
+extern fn ImGui_GetIO() ?*anyopaque;
+
+extern fn ImGui_NewFrame() callconv(.c) void;
+extern fn ImGui_Render() callconv(.c) void;
+
+extern fn ImGui_Text(fmt: [*c]const u8, ...) callconv(.c) void;
+
+const ImGuiIO = extern struct {
+    __pad0: [1]c_int,
+    BackendFlags: c_int,
+    DisplaySize_x: f32,
+    DisplaySize_y: f32,
+    __pad1: [2984]u8,
+};
 
 pub fn main() void {
     ImGui_CreateContext(null);
 
-    // const io = Raw.ImGui_GetIO();
-    // io.BackendFlags.ImGuiBackendFlags_RendererHasTextures = true;
+    const io = ImGui_GetIO(); // 3000 bytes
+    const io_f: *ImGuiIO = @ptrCast(@alignCast(io));
+    io_f.DisplaySize_x = 1920;
+    io_f.DisplaySize_y = 1080;
+    io_f.BackendFlags |= 1 << 4;
 
     // var f: f32 = 0;
     for (0..20) |n| {
         print("NewFrame() {}\n", .{n});
-        // io.DisplaySize = .{ .x = 1920, .y = 1080 };
         ImGui_NewFrame();
 
-        // Raw.ImGui_Text("Hello, world!"); // NOTE: That 'w' should be capitalized :(
+        ImGui_Text("Hello, World!");
         // _ = Raw.ImGui_SliderFloat("float", &f, 0.0, 1.0, null, 0);
         // Raw.ImGui_Text(
         //     "Application average %.3f ms/frame (%.1f FPS)",
